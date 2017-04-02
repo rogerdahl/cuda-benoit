dd8dc150ac16e5562c330f67797735ebfa93d39f
Roger Dahl <git@dahlsys.com>
2017-04-02 08:08:46 -0600

    Delete object files

---
+++ cmake-build-debug/CMakeFiles/cuda_benoit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_benoit.dir/main.cpp.o"
+  "cuda_benoit.pdb"
+  "cuda_benoit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cuda_benoit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cuda_benoit.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cuda_benoit.
+# This may be replaced when dependencies are built.
